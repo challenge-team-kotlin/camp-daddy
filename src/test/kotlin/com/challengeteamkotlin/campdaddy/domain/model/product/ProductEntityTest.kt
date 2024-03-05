16d15ec3166edf31ce7e1f90e59cb1a10647f621
zunbeeok <zunbeeok@gmail.com>
2024-03-05 16:31:41 +0900

    ✅ Test: 상품 Entity 단위 테스트코드 추가

--- src/test/kotlin/com/challengeteamkotlin/campdaddy/domain/model/product/ProductEntityTest.kt
+++ src/test/kotlin/com/challengeteamkotlin/campdaddy/domain/model/product/ProductEntityTest.kt
@@ -3,9 +3,11 @@ package com.challengeteamkotlin.campdaddy.domain.model.product
 import io.kotest.core.spec.style.BehaviorSpec
 import com.challengeteamkotlin.campdaddy.fixture.product.ProductFixture.lamp
 import com.challengeteamkotlin.campdaddy.fixture.product.ProductFixture.tent
+import com.challengeteamkotlin.campdaddy.fixture.product.ProductFixture.tent1
 import com.challengeteamkotlin.campdaddy.fixture.product.ProductImageFixture.productImage1
 import com.challengeteamkotlin.campdaddy.fixture.product.ProductImageFixture.productImage2
 import com.challengeteamkotlin.campdaddy.fixture.product.ProductImageFixture.productImage3
+import com.challengeteamkotlin.campdaddy.fixture.member.MemberFixture.seller
 import io.kotest.matchers.shouldBe
 
 class ProductEntityTest : BehaviorSpec({
@@ -22,6 +24,12 @@ class ProductEntityTest : BehaviorSpec({
                 tent.uploadImage(productImage2)
                 tent.uploadImage(productImage3)
                 tent.images.size shouldBe 2
+            }
+        }
+
+        When("이미지가 없이 생성된다면?"){
+            Then("게시글이 생성된다."){
+                tent1.memberEntity shouldBe seller
             }
         }
     }
--- src/test/kotlin/com/challengeteamkotlin/campdaddy/fixture/product/ProductFixture.kt
+++ src/test/kotlin/com/challengeteamkotlin/campdaddy/fixture/product/ProductFixture.kt
@@ -7,5 +7,6 @@ import com.challengeteamkotlin.campdaddy.fixture.member.MemberFixture.seller
 object ProductFixture {
     val lamp = ProductEntity(seller, 20000, "불이 잘 들어오는 램프", "잘 들어 옵니다.", Category.LAMPS)
     val tent = ProductEntity(seller, 100_000, "텐트 빌려드려요", "텐트 빌려드림ㅇㅇ", Category.TENTS)
+    val tent1 = ProductEntity(seller, 100_000, "군대 A형 텐트", "군대에서 전역할때 훔쳐온 A형 텐트입니다.", Category.TENTS)
 
 }
