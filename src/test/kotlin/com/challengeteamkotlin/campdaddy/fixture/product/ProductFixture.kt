6cb9170cc66c6d1413df4487c0c16adf0318b1e4
seunghwan <lovelyunsh@naver.com>
2024-03-05 14:51:41 +0900

    ✅ Test: 리뷰 Entity 단위 테스트 작성 중 변경된 ProductFixture 복구

--- src/test/kotlin/com/challengeteamkotlin/campdaddy/fixture/product/ProductFixture.kt
+++ src/test/kotlin/com/challengeteamkotlin/campdaddy/fixture/product/ProductFixture.kt
@@ -5,5 +5,5 @@ import com.challengeteamkotlin.campdaddy.domain.model.product.ProductEntity
 import com.challengeteamkotlin.campdaddy.fixture.member.MemberFixture.seller
 
 object ProductFixture {
-    val tent = ProductEntity(seller, 100_000, "텐트 빌려드려요", "텐트 빌려드림ㅇㅇ", Category.TENTS)
+    val tent = ProductEntity(seller, 100_000, null, "텐트 빌려드려요", "텐트 빌려드림ㅇㅇ", Category.TENTS)
 }
