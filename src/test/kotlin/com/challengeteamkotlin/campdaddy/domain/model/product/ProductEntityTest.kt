0abc5212ad7bbcb95d195af4b0357c3739aa41c9
zunbeeok <zunbeeok@gmail.com>
2024-03-05 16:23:25 +0900

    ✅ Test: 상품 Entity 단위 테스트코드 추가

--- src/test/kotlin/com/challengeteamkotlin/campdaddy/domain/model/product/ProductEntityTest.kt
+++ src/test/kotlin/com/challengeteamkotlin/campdaddy/domain/model/product/ProductEntityTest.kt
@@ -2,8 +2,10 @@ package com.challengeteamkotlin.campdaddy.domain.model.product
 
 import io.kotest.core.spec.style.BehaviorSpec
 import com.challengeteamkotlin.campdaddy.fixture.product.ProductFixture.lamp
+import com.challengeteamkotlin.campdaddy.fixture.product.ProductFixture.tent
 import com.challengeteamkotlin.campdaddy.fixture.product.ProductImageFixture.productImage1
 import com.challengeteamkotlin.campdaddy.fixture.product.ProductImageFixture.productImage2
+import com.challengeteamkotlin.campdaddy.fixture.product.ProductImageFixture.productImage3
 import io.kotest.matchers.shouldBe
 
 class ProductEntityTest : BehaviorSpec({
@@ -17,9 +19,9 @@ class ProductEntityTest : BehaviorSpec({
 
         When("여러이미지를 올린다면?") {
             Then("게시글이 생성된다.") {
-                lamp.uploadImage(productImage1)
-                lamp.uploadImage(productImage2)
-                lamp.images.size shouldBe 2
+                tent.uploadImage(productImage2)
+                tent.uploadImage(productImage3)
+                tent.images.size shouldBe 2
             }
         }
     }
--- src/test/kotlin/com/challengeteamkotlin/campdaddy/fixture/product/ProductImageFixture.kt
+++ src/test/kotlin/com/challengeteamkotlin/campdaddy/fixture/product/ProductImageFixture.kt
@@ -2,9 +2,10 @@ package com.challengeteamkotlin.campdaddy.fixture.product
 
 import com.challengeteamkotlin.campdaddy.domain.model.product.ProductImageEntity
 import com.challengeteamkotlin.campdaddy.fixture.product.ProductFixture.lamp
+import com.challengeteamkotlin.campdaddy.fixture.product.ProductFixture.tent
 
 object ProductImageFixture {
     val productImage1 = ProductImageEntity(lamp, "testImage1")
-    val productImage2 = ProductImageEntity(lamp, "testImage2")
-
+    val productImage2 = ProductImageEntity(tent, "testImage2")
+    val productImage3 = ProductImageEntity(tent, "testImage2")
 }
