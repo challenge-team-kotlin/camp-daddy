820b3ef882c4abadd36bd7de21403c9037389c4a
이승상 <soang2533@gmail.com>
2024-03-07 16:50:08 +0900

    ✨ Feat: 회원 CRUD & security, oauth2 kakao 로그인 변경 3 - test code 변경

--- src/test/kotlin/com/challengeteamkotlin/campdaddy/domain/model/member/MemberEntityTest.kt
+++ src/test/kotlin/com/challengeteamkotlin/campdaddy/domain/model/member/MemberEntityTest.kt
@@ -1,13 +1,21 @@
 package com.challengeteamkotlin.campdaddy.domain.model.member
 
+import com.challengeteamkotlin.campdaddy.fixture.member.MemberEntityFixture.testPerson
 import io.kotest.core.spec.style.BehaviorSpec
+import io.kotest.matchers.shouldBe
 
 
 class MemberEntityTest : BehaviorSpec({
 
-    Given("") {
-        When("") {
-            Then("") {
+    Given("유저 생성 확인") {
+        When("생성 된 유저가 있다면") {
+            Then("생성 된 유저와 등록 된 값은 같다") {
+                testPerson.email shouldBe "test@kakao.com"
+                testPerson.nickname shouldBe "test"
+                testPerson.name shouldBe "test"
+                testPerson.phoneNumber shouldBe "010-1234-1234"
+                testPerson.provider shouldBe MemberProvider.KAKAO
+                testPerson.providerId shouldBe "1234test"
             }
         }
     }
