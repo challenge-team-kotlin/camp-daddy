338c77752c8451085263d0c6891e0b2846160e64
이승상 <soang2533@gmail.com>
2024-03-07 16:17:29 +0900

    ✨ Feat: 회원 CRUD & security, oauth2 kakao 로그인 변경 2 - yml 변경 - pr 피드백 변경 - test code 변경

--- src/test/kotlin/com/challengeteamkotlin/campdaddy/fixture/member/MemberFixture.kt
+++ src/test/kotlin/com/challengeteamkotlin/campdaddy/fixture/member/MemberFixture.kt
@@ -1,9 +1,10 @@
 package com.challengeteamkotlin.campdaddy.fixture.member
 
 import com.challengeteamkotlin.campdaddy.domain.model.member.MemberEntity
+import com.challengeteamkotlin.campdaddy.domain.model.member.MemberProvider
 
 
 object MemberFixture {
-    val buyer = MemberEntity("buyer@google.com", "buyer", "백다삼", "01012345678")
-    val seller = MemberEntity("seller@google.com", "seller", "김다팜", "01087654321")
+    val buyer = MemberEntity("buyer@google.com", "buyer", "백다삼", "01012345678", MemberProvider.KAKAO, "1234test")
+    val seller = MemberEntity("seller@google.com", "seller", "김다팜", "01087654321", MemberProvider.KAKAO, "1234test")
 }
