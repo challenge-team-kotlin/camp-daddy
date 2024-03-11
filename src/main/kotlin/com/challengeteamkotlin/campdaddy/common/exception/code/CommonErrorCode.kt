7bf9b7b4f0c92ebb08b180aa6100fef612065ea2
zunbeeok <zunbeeok@gmail.com>
2024-03-11 09:54:58 +0900

    ✨ Feat: 상품 도메인 CRUD
    - S3 생성 코드 제거.

--- src/main/kotlin/com/challengeteamkotlin/campdaddy/common/exception/code/CommonErrorCode.kt
+++ src/main/kotlin/com/challengeteamkotlin/campdaddy/common/exception/code/CommonErrorCode.kt
@@ -10,8 +10,6 @@ enum class CommonErrorCode(
     VALIDATION_FAILED(9000, HttpStatus.BAD_REQUEST),
     ACCESS_DENIED(9001, HttpStatus.UNAUTHORIZED, "권한이 없습니다."),
     ID_NOT_FOUND(9002, HttpStatus.BAD_REQUEST, "조회 된 회원이 없습니다."),
-    AWS_IMAGE_UPLOAD_FAIL(9003,HttpStatus.BAD_REQUEST,"이미지 업로드에 실패했습니다."),
-    AWS_IMAGE_NAME_FAIL(9004,HttpStatus.BAD_REQUEST,"지원하지 않는 확장자 입니다."),
     ;
     constructor(id: Long, status: HttpStatus) : this(id, status, null)
 }
--- src/main/kotlin/com/challengeteamkotlin/campdaddy/infrastructure/aws/exception/AwsException.kt
+++
@@ -1,7 +0,0 @@
-package com.challengeteamkotlin.campdaddy.infrastructure.aws.exception
-
-import com.challengeteamkotlin.campdaddy.common.exception.CustomException
-import com.challengeteamkotlin.campdaddy.common.exception.code.ErrorCode
-import kotlin.reflect.KClass
-
-class AwsException(errorCode: ErrorCode):CustomException(errorCode)
